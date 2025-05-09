8343ffcbedf5c7e596787ab959bc95399cb0fa27
GuillaumeVDM <vandemoorteleguillaume@gmail.com>
2025-05-09 22:51:42 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,1 @@
-# test/conftest.py
-import sys, os
 
-# Voeg de project-root (één niveau hoger) toe aan de import-path
-root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-if root not in sys.path:
-    sys.path.insert(0, root)
